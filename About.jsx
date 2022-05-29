9b4b6334f9f51f2b8f74d0b193bb02071ef2c058
amarsanaa1234 <66049503+amarsanaa1234@users.noreply.github.com>
2022-05-29 13:34:51 +0800

    about
    ab

---
+++ About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './about.css'
+
+
+const About = () => {
+  return (
+    <about>
+      <div className='title'>
+        <h1>What is</h1>
+      </div>
+      <div className='logo'>
+        <p>Монгол Хөгжүүлэгч Нэгдэл</p>
+      </div>
+      <div className='mini_title'>
+        <h5>TechStart</h5>
+      </div>
+      <div className='about'>
+        <p>Бид технологийн салбартаа хувь нэмэр үруулж буй залуусын хүрээлэл ба дараагийн үеийн  залуусынхаа хөгжлийн гүүр нь болохыг зорьдог.</p>
+      </div>
+    </about>
+  )
+}
+
+export default About
